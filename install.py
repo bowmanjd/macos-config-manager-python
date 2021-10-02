b60ab3b098b2bd3ec2e1037d59d6e8d6c10a021f
Jonathan Bowman <git@bowmanjd.com>
2021-10-02 06:32:52 -0400

    style: adding docs and flake8 support

--- install.py
+++ install.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Install my desired packages."""
 
 import json
 import pathlib
@@ -6,53 +7,86 @@ import re
 import shlex
 import subprocess
 
+BREW = "/usr/local/bin/brew"
+NPM = "/usr/local/bin/npm"
 
-def read_list_from_file(filename):
+
+def read_list_from_file(filename: str) -> set:
+    """Build a set from a simple multiline text file.
+
+    Args:
+        filename: name of the text file
+
+    Returns:
+        a set of the unique lines from the file
+    """
     filepath = pathlib.Path(filename)
     lines = filepath.read_text().splitlines()
     return set(lines)
 
 
-def read_formulae():
-    leaves_list = subprocess.check_output(["brew", "leaves"], text=True).splitlines()
+def read_formulae() -> set:
+    """Obtain Homebrew formulae that are not dependencies of another formula.
+
+    Returns:
+        a set of formulae names
+    """
+    leaves_list = subprocess.check_output([BREW, "leaves"], text=True).splitlines()
     return set(leaves_list)
 
 
-def read_casks():
+def read_casks() -> set:
+    """Obtain list of casks installed with Homebrew.
+
+    Returns:
+        a set of cask names
+    """
     cask_list = subprocess.check_output(
-        ["brew", "list", "--cask", "-1"], text=True
+        [BREW, "list", "--cask", "-1"], text=True
     ).splitlines()
     return set(cask_list)
 
 
-def read_taps():
-    tap_list = subprocess.check_output(["brew", "tap"], text=True).splitlines()
+def read_taps() -> set:
+    """Obtain list of utilized Homebrew taps.
+
+    Returns:
+        a set of taps
+    """
+    tap_list = subprocess.check_output([BREW, "tap"], text=True).splitlines()
     return set(tap_list)
 
 
-def read_npm():
+def read_npm() -> set:
+    """Obtain list of globally-installed npm packages.
+
+    Returns:
+        a set of package names
+    """
     pkg_json = subprocess.check_output(
-        ["npm", "list", "-g", "--depth", "0", "--json"], text=True
+        [NPM, "list", "-g", "--depth", "0", "--json"], text=True
     )
     packages = json.loads(pkg_json)
     return set(packages["dependencies"])
 
 
-def install_npm_packages():
+def install_npm_packages() -> None:
+    """Install npm packages from text file."""
     to_install = read_list_from_file("npm.txt")
     existing = read_npm()
-    cmd = ["npm", "install", "--global"]
+    cmd = [NPM, "install", "--global"]
     for line in to_install - existing:
         subprocess.check_call(cmd + shlex.split(line))
 
 
-def install_brew():
+def install_brew() -> None:
+    """Install Homebrew if not installed already."""
     try:
-        subprocess.check_output(["which", "brew"])
+        subprocess.check_output(["/usr/bin/which", "brew"])
     except subprocess.CalledProcessError:
         script = subprocess.check_output(
             [
-                "curl",
+                "/usr/bin/curl",
                 "-fsSL",
                 "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh",
             ]
@@ -60,26 +94,29 @@ def install_brew():
         subprocess.check_call(["/bin/bash", "-c", script])
 
 
-def install_taps():
+def install_taps() -> None:
+    """Install external Homebrew taps from text file."""
     to_install = read_list_from_file("taps.txt")
     existing = read_taps()
-    cmd = ["brew", "tap"]
+    cmd = [BREW, "tap"]
     for line in to_install - existing:
         subprocess.check_call(cmd + shlex.split(line))
 
 
-def install_formulae():
+def install_formulae() -> None:
+    """Install Homebrew formulae from text file."""
     to_install = read_list_from_file("formulae.txt")
     existing = read_formulae()
-    cmd = ["brew", "install"]
+    cmd = [BREW, "install"]
     for line in to_install - existing:
         subprocess.check_call(cmd + shlex.split(line))
 
 
-def install_casks():
+def install_casks() -> None:
+    """Install Homebrew casks from text file and allow through Gatekeeper."""
     to_install = read_list_from_file("casks.txt")
     existing = read_casks()
-    cmd = ["brew", "install"]
+    cmd = [BREW, "install"]
     app_pattern = re.compile("'/Applications/[^']+'")
     for line in to_install - existing:
         print(f"Installing {line}")
@@ -96,14 +133,19 @@ def install_casks():
             )
 
 
-def read_mas():
-    sep = re.compile("\s{2,}")
+def read_mas() -> set:
+    """Read list of App store packages to install.
+
+    Returns:
+        set of packages
+    """
+    sep = re.compile(r"\s{2,}")
     raw_list = subprocess.check_output(["mas", "list"], text=True)
-    # apps = {k:v for k,v in (i.split()[0:2] for i in raw_list.splitlines())}
-    return set(sep.split(i, maxsplit=1)[0] for i in raw_list.splitlines())
+    return {sep.split(i, maxsplit=1)[0] for i in raw_list.splitlines()}
 
 
-def install_mas():
+def install_mas() -> None:
+    """Install App store packages from text file."""
     app_list = pathlib.Path("mas.json")
     to_install = set(json.loads(app_list.read_text()))
     existing = read_mas()
@@ -111,7 +153,8 @@ def install_mas():
         subprocess.check_call(["mas", "install", line])
 
 
-def run():
+def run() -> None:
+    """Run commands in order."""
     install_brew()
     install_taps()
     install_formulae()
